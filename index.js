06aceeb1abc5c90bfa4d47b0302585053210d0a4
Rasleen Kaur <rasleen0209@gmail.com>
2026-05-31 13:41:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const recommendationRoutes = require("./routes/recommendationRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use("/api/recommendations", recommendationRoutes);
 
