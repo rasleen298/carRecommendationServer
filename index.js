2fac2f8c1be9eb43f4ae2914211f08745fea5170
Rasleen Kaur <rasleen0209@gmail.com>
2026-05-31 13:39:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,4 +16,7 @@ app.get("/api/cars", (req, res) => {
   );
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
